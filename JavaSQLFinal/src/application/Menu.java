ed2f62394f6dada99851e08eb4afd2ba540f0f32
Lisa <las619@comcast.net>
2021-01-20 12:39:06 -0500

    Add scanner code for Album methods
    Also fixed workings of the sub menus -- we can now choose to be in the Artist Menu, and stay there for multiple operations, this makes it easier to test our code.

--- JavaSQLFinal/src/application/Menu.java
+++ JavaSQLFinal/src/application/Menu.java
@@ -42,7 +42,9 @@ public class Menu {
 	
 
 			do {
-				System.out.println("\n\nMain " + DATABASE_NAME + " Menu\n");
+				System.out.println("---------------------------");
+				System.out.println("MAIN " + DATABASE_NAME + " MENU");
+				System.out.println("---------------------------");
 				printMenu(options);
 				scanner = new Scanner(System.in);
 				selection = scanner.nextLine();
@@ -50,86 +52,91 @@ public class Menu {
 				try {
 					
 					if (selection.equals("1") ) {
-						System.out.println("\nAlbum Information Menu\n");
-						printMenu(album_options);
-						scanner = new Scanner(System.in);
-						subselection = scanner.nextLine();
-
-						
-						if (subselection.equals("1") ) {
-							System.out.println("\tDisplaying all albums...");
-							displayAllAlbums();
-						} else if (subselection.equals("2") ) {
-							System.out.println("\tAdding an album...");
-							addNewAlbum();
-						} else if (subselection.equals("3") ) {
-							System.out.println("\tDeleting an album...");
-							deleteAlbum();
-						} else if (subselection.equals("4") ) {
-							System.out.println("\tUpdating an album...");
-							updateAlbum();
-						} else {
-							if (!(subselection.equals("-1"))) {
-								System.out.println("Invalid Option!");
+						System.out.println("----------------------");
+						System.out.println("ALBUM Information Menu");
+						System.out.println("----------------------");
+						do {
+							printMenu(album_options);
+							scanner = new Scanner(System.in);
+							subselection = scanner.nextLine();
+					
+							if (subselection.equals("1") ) {
+								System.out.println("\tDisplaying all albums...\n");
+								displayAllAlbums();
+							} else if (subselection.equals("2") ) {
+								System.out.println("\tAdding an album...\n");
+								addNewAlbum();
+							} else if (subselection.equals("3") ) {
+								System.out.println("\tDeleting an album...\n");
+								deleteAlbum();
+							} else if (subselection.equals("4") ) {
+								System.out.println("\tUpdating an album...\n");
+								updateAlbum();
+							} else if (!(subselection.equals("-1"))) {
+									System.out.println("Invalid Option!");
 							}
-							
-						}	
+								
+						} while (!(subselection.equals("-1")));	
 						
 					} else if (selection.equals("2") ) {
-						
-						System.out.println("Artist Information Menu\n");
-						printMenu(artist_options);
-						scanner = new Scanner(System.in);
-						subselection = scanner.nextLine();
+						System.out.println("-----------------------");
+						System.out.println("ARTIST Information Menu");
+						System.out.println("-----------------------");
 
-					    if (subselection.equals("1") ) {
-					    	System.out.println("\tDisplaying all artists...");
-							displayAllArtists();						
-						} else if (subselection.equals("2") ) {
-							System.out.println("\tAdding an artist...");
-							addNewArtist();		
-						} else if (subselection.equals("3") ) {
-							System.out.println("\tDeleting an artist...");
-							deleteArtist();		
-						} else if (subselection.equals("4") ) {
-							System.out.println("\tUpdating an artist...");
-							updateArtist();					
-						} else {
-							if (!(subselection.equals("-1"))) {
-								System.out.println("Invalid Option!");
+						do {
+							printMenu(artist_options);
+							scanner = new Scanner(System.in);
+							subselection = scanner.nextLine();
+
+						    if (subselection.equals("1") ) {
+						    	System.out.println("\tDisplaying all artists...\n");
+								displayAllArtists();						
+							} else if (subselection.equals("2") ) {
+								System.out.println("\tAdding an artist...\n");
+								addNewArtist();		
+							} else if (subselection.equals("3") ) {
+								System.out.println("\tDeleting an artist...\n");
+								deleteArtist();		
+							} else if (subselection.equals("4") ) {
+								System.out.println("\tUpdating an artist...\n");
+								updateArtist();					
+							} else if (!(subselection.equals("-1"))) {
+									System.out.println("Invalid Option!");
 							}
-						}	
-					
+						} while (!(subselection.equals("-1")));
+													
 						
 					} else if (selection.equals("3") ) {
-						System.out.println("Certification Information Menu\n");
-						printMenu(cert_options);
-						scanner = new Scanner(System.in);
-						subselection = scanner.nextLine();
-						
-						if (subselection.equals("1") ) {
-							System.out.println("\tDisplaying all certifications...");
-							displayAllCerts();		
-						} else if (subselection.equals("2") ) {
-							System.out.println("\tAdding a certification...");
-							addNewCert();		
-						} else if (subselection.equals("3") ) {
-							System.out.println("\tDeleting a certification...");
-							deleteCert();		
-						} else if (subselection.equals("4") ) {
-							System.out.println("\tUpdating a certification...");
-							updateCert();		
-						} else {
-							if (!(subselection.equals("-1"))) {
-									System.out.println("Invalid Option!");
-							}		
-						}
+						System.out.println("------------------------------");
+						System.out.println("CERTIFICATION Information Menu");
+						System.out.println("------------------------------");
+
+						do {
+							printMenu(cert_options);
+							scanner = new Scanner(System.in);
+							subselection = scanner.nextLine();
+							
+							if (subselection.equals("1") ) {
+								System.out.println("\tDisplaying all certifications...\n");
+								displayAllCerts();		
+							} else if (subselection.equals("2") ) {
+								System.out.println("\tAdding a certification...\n");
+								addNewCert();		
+							} else if (subselection.equals("3") ) {
+								System.out.println("\tDeleting a certification...\n");
+								deleteCert();		
+							} else if (subselection.equals("4") ) {
+								System.out.println("\tUpdating a certification...\n");
+								updateCert();		
+							} else if (!(subselection.equals("-1"))) {
+										System.out.println("Invalid Option!");		
+							}
+							
+						} while (!(subselection.equals("-1")));
 						
 
-					} else {
-						if (!(selection.equals("-1"))) {
-							System.out.println("Invalid Option!");
-						}	
+					} else if (!(selection.equals("-1"))) {
+							System.out.println("Invalid Option!");	
 					} 
 					
 				} catch(Exception e) {
@@ -257,7 +264,7 @@ public class Menu {
 			System.out.println("Delete Artist not performed!");
 		} else {
 			System.out.println("Id: " + artist.getArtist_id() + " \tName: " + artist.getArtist_name());
-			System.out.println();
+			System.out.println("\t**  This will also delete all of the associated data in the " + DATABASE_NAME + " database!  **\n");
 			System.out.print("Would you like to proceed, yes or no?  ");
 			String response = scanner.nextLine();
 			if (response.equalsIgnoreCase("yes")) {
@@ -279,15 +286,21 @@ public class Menu {
 		String artistName = scanner.nextLine();
 		System.out.println("Updating Artist...");
 		Artist artist = artistDao.getArtistByName(artistName);
-		System.out.println("Id: " + artist.getArtist_id() + " \tName: " + artist.getArtist_name());	
-		System.out.println();
-		System.out.print("Enter NEW Name of Artist to Update: ");
-		String newName = scanner.nextLine();
-		if (newName == null) {
+		if (artist == null) {
+			System.out.println("Artist Not Found in database: " + DATABASE_NAME);
 			System.out.println("Update Artist not performed!");
+
 		} else {
-			artistDao.updateArtist(artistName,newName);
-		}	
+			System.out.println("Id: " + artist.getArtist_id() + " \tName: " + artist.getArtist_name());	
+			System.out.println();
+			System.out.print("Enter NEW Name of Artist to Update: ");
+			String newName = scanner.nextLine();
+			if (newName == null) {
+				System.out.println("Update Artist not performed!");
+			} else {
+				artistDao.updateArtist(artistName,newName);
+			}	
+		}
 	}
 
 	/*
@@ -354,7 +367,8 @@ public class Menu {
 	 * Method:  printMenu()
 	 */
 	private void printMenu(List<String> options) {
-		System.out.println("\tPlease select an option...\n\tAll requests are on the " + DATABASE_NAME + " database:\n------------------------------------------------------------");
+		System.out.println("\n------------------------------------------------------------");
+		System.out.println("\tPlease SELECT AN OPTION...\n\tAll requests are on the " + DATABASE_NAME + " database!\n------------------------------------------------------------");
 		for (int i = 0; i<options.size(); i++) {
 				System.out.println("   "+(i+1) + ") " + options.get(i));
 			}		
